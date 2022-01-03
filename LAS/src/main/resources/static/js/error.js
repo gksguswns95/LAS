32173c0c867ef46b094203d4a477b23a7067cdc5
rhsl3817 <rhsl3817@gmail.com>
2022-01-03 16:55:11 +0900

    2022-01-03 김찬호 error.js 수정

--- LAS/src/main/resources/static/js/error.js
+++ LAS/src/main/resources/static/js/error.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	if($('#error').val() == 'loginError') {
 		$('#fail').html("아이디 또는 비밀번호가 잘못 입력 되었습니다.<br> 아이디와 비밀번호를 정확히 입력해 주세요.");
-		$('#section').css('height','680px')
+		$('#section').css('height','520px')
 	}
 	
 	if($('#error').val() == 'withdrawalAccount') {
 		$('#fail').html("회원탈퇴하여 로그인이 불가능합니다.");
-		$('#section').css('height','680px')
+		$('#section').css('height','520px')
 	}
 	
 	if($('#error').val() == 'passwordConfirmError' || $('#error').val() == 'withdrawalAccountError') {
